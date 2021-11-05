b098c8bc5af527a4744ed92ee554ab2796bf66f6
Jose <jquintans@outlook.com>
2021-11-05 23:32:37 +0100

    cleanup comments

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ int main(int argc, char *argv[])
   int lower_boundary = atoi(argv[2]);
   int upper_boundary = atoi(argv[3]);
   BinarySearch binarySearch(lower_boundary, upper_boundary);
-  // binarySearch.setArray(atoi(argv[2]), atoi(argv[3]));
   unsigned int index = binarySearch.getIndexOf(target);
   std::cout << "index: " << index << std::endl;
   return 0;

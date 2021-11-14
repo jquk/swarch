14b5f616d6725652efcc2cec12f2d5fa9f2cbea4
Jose <jquintans@outlook.com>
2021-11-14 02:59:46 +0100

    minor change

--- Graph/BreadthFirstSearch.cpp
+++ Graph/BreadthFirstSearch.cpp
@@ -1,11 +1,71 @@
 #include "BreadthFirstSearch.h"
 
+/*
+*
+*/
 BreadthFirstSearch::BreadthFirstSearch()
 {
 
 }
 
+/*
+*
+*/
+BreadthFirstSearch::BreadthFirstSearch(std::vector<std::vector<unsigned int>>connectionsMatrix)
+: m_connectionsMatrix(connectionsMatrix)
+{
+
+}
+
+/*
+*
+*/
 BreadthFirstSearch::~BreadthFirstSearch()
 {
 
+}
+
+/*
+*
+*/
+std::vector<unsigned int> BreadthFirstSearch::getShortestPath(unsigned int startingNode, unsigned int targetNode)
+{
+  std::vector<unsigned int> shortestPath = {0, 1, 2, 3};
+  return shortestPath;
+}
+
+/*
+*
+*/
+std::vector<unsigned int> BreadthFirstSearch::getShortestPath(std::vector<std::vector<unsigned int>> graph, unsigned int startingNode, unsigned int targetNode)
+{
+  std::vector<unsigned int> shortestPath = {0, 1, 2, 3};
+  return shortestPath;
+}
+
+/*
+*
+*/
+void BreadthFirstSearch::setCurrentNode()
+{
+
+}
+
+/*
+*
+*/
+unsigned int BreadthFirstSearch::getCurrentNode()
+{
+  return 0;
+}
+
+/*
+*
+*/
+int BreadthFirstSearch::getNeighborsOfCurrentNode(void)
+{
+  // for (unsigned int i = 0; i < this->m_currentNode.; i++) {
+  //   this->m_neighborNodesQueue;
+  // }
+  return 0;
 }
--- Graph/BreadthFirstSearch.h
+++ Graph/BreadthFirstSearch.h
@@ -1,4 +1,5 @@
 #include <queue>
+
 /*
 * Implementation of the Breadth First Search algorithm.
 */
@@ -6,12 +7,23 @@ class BreadthFirstSearch {
 public:
   // constructor
   BreadthFirstSearch();
+  BreadthFirstSearch(std::vector<std::vector<unsigned int>>graph);
+
+
   // destructor
   ~BreadthFirstSearch();
 
   // member functions
+  std::vector<unsigned int> getShortestPath(unsigned int startingNode, unsigned int targetNode);
+  std::vector<unsigned int> getShortestPath(std::vector<std::vector<unsigned int>> graph, unsigned int startingNode, unsigned int targetNode);
 
 private:
   // member variables
-  std::queue<int>m_nodes;
+  unsigned int m_currentNode;
+  std::queue<int>m_neighborNodesQueue;
+  std::vector<std::vector<unsigned int>>m_connectionsMatrix;
+  // member functions
+  void setCurrentNode();
+  unsigned int getCurrentNode();
+  int getNeighborsOfCurrentNode(void);
 };

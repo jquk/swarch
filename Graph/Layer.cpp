dee18b47ba234ef0b3b86996dac34ad27f9f7c12
Jose <jquintans@outlook.com>
2021-11-19 00:52:42 +0100

    add Layer class

---
+++ Graph/Layer.cpp
@@ -0,0 +1,102 @@
+#include "Layer.h"
+
+/*
+*
+*/
+Layer::Layer(unsigned int layerId)
+: m_layerId(layerId)
+{
+
+}
+
+/*
+*
+*/
+Layer::~Layer()
+{
+
+}
+
+/*
+*
+*/
+unsigned int getLayerId()
+{
+  unsigned int layerId;
+  return layerId;
+}
+
+/*
+*
+*/
+void addNodeId(unsigned int nodeId)
+{
+
+}
+
+/*
+*
+*/
+void addNode(Node node)
+{
+
+}
+
+/*
+*
+*/
+std::vector<Node> getNodeById(unsigned int nodeId)
+{
+  std::vector<Node> node;
+  return node;
+}
+
+/*
+*
+*/
+std::vector<unsigned int> getNodesIds()
+{
+  std::vector<unsigned int> nodes;
+  return nodes;
+}
+
+/*
+*
+*/
+std::vector<Node> getNodes()
+{
+  std::vector<Node> node;
+  return node;
+}
+
+/*
+*
+*/
+void addNodesIds(std::vector<unsigned int> nodesIds)
+{
+
+}
+
+/*
+*
+*/
+void addNodes(std::vector<Node> nodes)
+{
+
+}
+
+/*
+*
+*/
+void removeNodesByIds(std::vector<unsigned int> nodesIds)
+{
+
+}
+
+/*
+*
+*/
+void removeNodes(std::vector<Node> nodes)
+{
+
+}
---
+++ Graph/Layer.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include "Node.h"
+
+/*
+*
+*/
+class Layer {
+public:
+  // constructor
+  Layer(unsigned int layerId);
+  // destructor
+  ~Layer();
+
+  // public member variables
+
+  // public member functions
+  unsigned int getLayerId();
+  void addNodeId(unsigned int nodeId);
+  void addNode(Node node);
+  std::vector<Node> getNodeById(unsigned int nodeId);
+  std::vector<unsigned int> getNodesIds();
+  std::vector<Node> getNodes();
+  void addNodesIds(std::vector<unsigned int> nodesIds);
+  void addNodes(std::vector<Node> nodes);
+  void removeNodesByIds(std::vector<unsigned int> nodesIds);
+  void removeNodes(std::vector<Node> nodes);
+
+private:
+  // private member variables
+  unsigned int m_layerId;
+  std::vector<Node> m_nodes;
+  // private member functions
+};

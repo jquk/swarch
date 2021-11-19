92412b685676ae5dba68e574d14c80aacb0e7f29
Jose <jquintans@outlook.com>
2021-11-19 03:23:32 +0100

    define functions, add m_nodeId

--- Graph/Node.cpp
+++ Graph/Node.cpp
@@ -24,3 +24,60 @@ Node::~Node()
 {
 
 }
+
+/*
+*
+*/
+bool Node::getExplored()
+{
+  return this->m_explored;
+}
+
+/*
+*
+*/
+void Node::setExplored(bool explored)
+{
+  this->m_explored = explored;
+}
+
+/*
+*
+*/
+unsigned int Node::getNodeId()
+{
+  return this->m_nodeId;
+}
+
+/*
+*
+*/
+void Node::setLayerId()
+{
+
+}
+
+/*
+*
+*/
+void Node::setNodeId(unsigned int nodeId)
+{
+
+}
+
+/*
+*
+*/
+std::vector<unsigned int> Node::getNeighbors()
+{
+  std::vector<unsigned int> retVal_neighbors = {};
+  return retVal_neighbors;
+}
+
+/*
+*
+*/
+void Node::setNeighbors(std::vector<unsigned int> neighbors)
+{
+
+}
--- Graph/Node.h
+++ Graph/Node.h
@@ -14,17 +14,19 @@ public:
   // public member variables
 
   // public member functions
-  bool hasBeenExplored();
+  bool getExplored();
+  void setExplored(bool explored);
   unsigned int getNodeId();
-  unsigned int setLayerId();
+  void setLayerId();
   void setNodeId(unsigned int nodeId);
   std::vector<unsigned int> getNeighbors();
   void setNeighbors(std::vector<unsigned int> neighbors);
 
 private:
   // private member variables
-  bool m_explored;
+  bool m_nodeId;
   unsigned int m_layerId;
   std::vector<unsigned int> m_neighbors;
+  bool m_explored;
   // private member functions
 };

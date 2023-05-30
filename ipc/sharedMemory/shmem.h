7e85c086d5b3bf8242809e0722bf0debb148562f
Jose <jquintans@outlook.com>
2023-05-30 16:59:47 +0200

    add IPC with shared memory examples

---
+++ ipc/sharedMemory/shmem.h
@@ -0,0 +1,5 @@
+#define BackingFile "tmp_backing_file.txt"
+#define AccessPerms S_IRUSR | S_IWUSR
+#define ByteSize 4096
+#define SemaphoreName "semaphore0"
+const char * MemContents = "These are the memory contents.";
